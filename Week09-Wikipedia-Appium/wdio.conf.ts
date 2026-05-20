969c7013bede18ae216665b74a9438edeccf6c1b
zynp-KC <kapacakzeynepp@gmail.com>
2026-05-21 02:07:58 +0300

    tests: add first Appium search test for Wikipedia Android app

---
+++ Week09-Wikipedia-Appium/wdio.conf.ts
@@ -0,0 +1,35 @@
+export const config = {
+    runner: 'local',
+    hostname: '127.0.0.1',
+    port: 4723,
+    path: '/',
+
+    specs: ['./tests/**/*.spec.ts'],
+    maxInstances: 1,
+
+    capabilities: [{
+        platformName: 'Android',
+        'appium:deviceName': 'RF8R200202M',
+        'appium:platformVersion': '12',
+        'appium:automationName': 'UiAutomator2',
+        'appium:app': 'C:\\Users\\ASUS\\Downloads\\app-alpha-universal-release.apk',
+        'appium:appPackage': 'org.wikipedia.alpha',
+        'appium:appActivity': 'org.wikipedia.main.MainActivity',
+        'appium:noReset': true,
+    }],
+
+    framework: 'mocha',
+    mochaOpts: {
+        timeout: 60000
+    },
+
+    autoCompileOpts: {
+        autoCompile: true,
+        tsNodeOpts: {
+            transpileOnly: true,
+            project: './tsconfig.json'
+        }
+    },
+
+    reporters: ['spec'],
+};
