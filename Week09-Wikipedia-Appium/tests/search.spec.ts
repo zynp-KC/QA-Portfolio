9e60bc9783cc247644580793bc9360cdf9407d9b
zynp-KC <kapacakzeynepp@gmail.com>
2026-05-21 23:17:42 +0300

    test: add beforeEach reset and invalid search test scenario

--- Week09-Wikipedia-Appium/tests/search.spec.ts
+++ Week09-Wikipedia-Appium/tests/search.spec.ts
@@ -2,12 +2,30 @@ import SearchPage from '../pages/SearchPage';
 
 describe('Wikipedia Search', () => {
 
-    it('should search for Appium and see results', async () => {
+    beforeEach(async () => {
+        await driver.terminateApp('org.wikipedia.alpha');
+        await driver.activateApp('org.wikipedia.alpha');
 
-        await SearchPage.skipOnboardingIfPresent();
+        try {
+            const skipBtn = await $('id=org.wikipedia.alpha:id/fragment_onboarding_skip_button');
+            await skipBtn.waitForDisplayed({ timeout: 3000 });
+            await skipBtn.click();
+        } catch (e) {
+        
+        }
+        await SearchPage.searchContainer.waitForDisplayed();
+    });
+
+    it('should search for Appium and see results', async () => {
         await SearchPage.search('Appium');
         await SearchPage.waitForResults();
-
         await expect(SearchPage.searchResults).toBeDisplayed();
     });
+
+    it('should show no results for invalid search term', async () => {
+        await SearchPage.search('xyzxyzxyz123');
+        await SearchPage.waitForResults();
+        await expect(SearchPage.searchResults).toBeDisplayed();
+    });
+
 });
